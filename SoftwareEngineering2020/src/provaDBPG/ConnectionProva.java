5bc3382e5c99b9c58d5a5bb39ac6d630f214b252
rodol <rodol@LAPTOP-SCG02AVT>
2020-11-29 20:23:36 +0100

    Query parametriche per Planned e Unplanned

--- SoftwareEngineering2020/src/provaDBPG/ConnectionProva.java
+++ SoftwareEngineering2020/src/provaDBPG/ConnectionProva.java
@@ -2,6 +2,7 @@ package provaDBPG;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
@@ -14,14 +15,21 @@ public class ConnectionProva {
         String url = "jdbc:postgresql://localhost/SoftwareEngineering";
         String user = "postgres";
         String pwd = "admin";
-        String query = "select * from mantainer";
+        
         try {
             Connection conn = DriverManager.getConnection(url, user, pwd);
             Class.forName("org.postgresql.Driver");
-            Statement stm = conn.createStatement();
-            ResultSet rst = stm.executeQuery(query);
-            while (rst.next()) {
-                System.out.println(rst.getString("nome").concat(" "+rst.getString("cognome")));
+            PreparedStatement stmt1 = conn.prepareStatement("select id_attivita_p,descrizione from planned where settimana=?");
+            stmt1.setInt(1, 12);
+            PreparedStatement stmt2 = conn.prepareStatement("select id_attivita_un,descrizione from unplanned where settimana=?");
+            stmt2.setInt(1, 3);
+            ResultSet rst1 = stmt1.executeQuery();
+            ResultSet rst2 = stmt2.executeQuery();
+            while (rst1.next()) {
+                System.out.println(rst1.getString("id_attivita_p").concat(" "+rst1.getString("descrizione")));
+            }
+            while (rst2.next()) {
+                System.out.println(rst2.getString("id_attivita_un").concat(" "+rst2.getString("descrizione")));
             }
             conn.close();
         } catch (java.sql.SQLException | ClassNotFoundException ex) {
