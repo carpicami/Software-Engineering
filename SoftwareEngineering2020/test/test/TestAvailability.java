812f695b6267d5492ee2dbc367f6bdc37dd0d485
rodol <rodol@LAPTOP-SCG02AVT>
2020-12-18 10:00:21 +0100

    Commit finale per DB, schemi di progettazione della Base dati e diagramma delle classi UML

--- SoftwareEngineering2020/test/test/TestAvailability.java
+++ SoftwareEngineering2020/test/test/TestAvailability.java
@@ -12,7 +12,6 @@ import java.sql.Time;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import org.graalvm.compiler.debug.Assertions;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
